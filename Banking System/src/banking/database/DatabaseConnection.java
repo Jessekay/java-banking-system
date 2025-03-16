5c9ba3e0f5e4dbcfbaf2cd2f33084699ee84e40b
jessekay <kayjes00@gmail.com>
2025-03-16 20:31:26 +0200

    add a database connection

--- Banking System/src/banking/database/DatabaseConnection.java
+++ Banking System/src/banking/database/DatabaseConnection.java
@@ -1,5 +1,15 @@
 package banking.database;
 
-public class DatabaseConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DatabaseConnection {
+    private static final String DB_URL = "jdbc:mysql://localhost/customer";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "12092001";
+
+    public static Connection connect() throws SQLException {
+        return DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+    }
 }
