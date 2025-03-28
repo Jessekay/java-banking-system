0355c6d718332d083f51857e4365b659e502d01f
jessekay <kayjes00@gmail.com>
2025-03-28 09:07:10 +0200

    create a class Customer to act as a model

---
+++ model/Customer.java
@@ -0,0 +1,66 @@
+public class Customer {
+    private String nationalID;
+    private String names;
+    private int age;
+    private String phoneNumber;
+    private String accountNumber;
+
+    public Customer (String nationalID, String names, int age, String phoneNumber, String accountNumber) {
+        this.nationalID = nationalID;
+        this.names = names;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+        this.accountNumber = accountNumber; 
+    }
+
+    public String getNationalID() {
+        return nationalID;
+    }
+
+    public void setNationalID(String nationalID) {
+        this.nationalID = nationalID;
+    }
+
+    public String getNames() {
+        return names;
+    }
+
+    public void setNames(String names) {
+        this.names = names;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "National ID: " + nationalID + "\n" +
+               "Names: " + names + "\n" +
+               "Age: " + age + "\n" +
+               "Phone number: " + phoneNumber + "\n" +
+               "Account Number: " + accountNumber;
+    }
+
+    
+}
